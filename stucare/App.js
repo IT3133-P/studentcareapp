1b3eb3fb280033381050ef97bbb0b0724327bc95
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-01-03 01:56:09 +0530

    feat: update app

--- stucare/App.js
+++ stucare/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Login from './page/loginPage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Login />
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
